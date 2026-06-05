66991d55fb2c16c723d182ef2fb74db7a138409f
happyheadhigh <jvweb3@gmail.com>
2026-06-05 19:57:37 -0400

    Move TraitView app runtime state into appState file

--- js/app.js
+++ js/app.js
@@ -5,20 +5,6 @@
 /* live settings */
 
 
-/* caches + state */
-let MANIFEST=null, CHUNK_SIZE=1000, CHUNKS_DIR='traits_chunks', TOKEN_COUNT=0;
-const CHUNK_CACHE=new Map(), ROW_CACHE=new Map();
-let TRAIT_FREQ={}, TRAIT_DOMAIN={}, MAX_TRAIT_COUNT=0;
-let currentTraitCount=null, activeTraits=new Map(), AVAILABLE_DOMAIN=null;
-let RARITY_OBS_RANK=new Map(), RARITY_THEO_RANK=new Map(), RARITY_MODE='observed', PROB_DATA=null;
-let OS_RANK_MAP = new Map(); // token_id → os_rank from Railway DB
-let pinnedA=null, pinnedB=null, pinnedSet=[]; const OPEN_GROUPS=new Set();
-let CHART_ID_MAP={};
-let rankMin=null, rankMax=null;
-
-
-const LAST_SALE_CACHE = new Map();
-const LAST_SALE_PENDING = new Set();
 
 function formatRelativeAgeFromTs(ts){
   const num = Number(ts);
